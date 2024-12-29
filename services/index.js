a551e95c05c9ca2b70c61e1db6a07cb7e6d87488
Rene-Roscher <roscher794@gmail.com>
2024-12-29 16:04:08 +0100

    some

--- services/index.js
+++ services/index.js
@@ -9,12 +9,15 @@ export default {
 
         // Update system metrics
         setImmediateInterval(() => SystemMonitorService.updateAllSystemMetrics(apiInstance), 10*1000);
-
-        // Run speed test
+        //
+        // // Run speed test
         setImmediateInterval(() => SpeedTestService.runSpeedTest(apiInstance), 60*60*1000);
+        //
+        // // Update GitHub stats
+        setImmediateInterval(() => GitHubStatsService.updateGitHubStats(apiInstance, 'Rene-Roscher'), 20*60*1000);
 
-        // Update GitHub stats
-        setImmediateInterval(() => GitHubStatsService.updateGitHubStats(apiInstance, 'TaylorOtwell'), 10*60*1000);
+        // // Update GitHub contributions
+        // setImmediateInterval(() => GitHubContributionsService.updateGitHubContributions(apiInstance, 'Rene-Roscher'), 20*60*1000);
 
         console.log('Services started.');
     }
