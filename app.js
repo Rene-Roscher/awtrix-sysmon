e198479f7aaaff4bd296adcc56c26619121c45d8
Rene-Roscher <roscher794@gmail.com>
2024-12-29 17:45:14 +0100

    added workflow to build & release

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const AwtrixAPI = require('./api/awtrix.js');
+const EffectService = require('./api/effects.js');
 const Service = require('./services/index.js');
 
 const api = new AwtrixAPI('http://192.168.0.73')
 
 Service.init(api);
+
+const effectService = new EffectService(api);
+
+effectService.applyEffect('LookingEyes', {
+    speed: 100,
+    blend: true,
+    duration: 15,
+    stack: false
+});
+
+
+
+
--- services/index.js
+++ services/index.js
@@ -5,17 +5,24 @@ const GitHubStatsService = require('./GitHubStatsService.js');
 const { setImmediateInterval } = require('../utils/Timings.js');
 
 module.exports = {
-    init: async (apiInstance) => {
+    init: async (apiInstance, services = {
+        resourceMonitor: true,
+        speedTest: false,
+        githubStats: true,
+    }) => {
         console.log('Starting services...');
 
         // Update system metrics
-        setImmediateInterval(() => SystemMonitorService.updateAllSystemMetrics(apiInstance), 5 * 1000);
+        if (services.resourceMonitor)
+            setImmediateInterval(() => SystemMonitorService.updateAllSystemMetrics(apiInstance), 5 * 1000);
 
         // Run speed test
-        // setImmediateInterval(() => SpeedTestService.runSpeedTest(apiInstance), 60 * 60 * 1000);
+        // if (services.speedTest)
+            // setImmediateInterval(() => SpeedTestService.runSpeedTest(apiInstance), 60 * 60 * 1000);
 
         // Update GitHub stats
-        setImmediateInterval(() => GitHubStatsService.updateGitHubStats(apiInstance, 'Rene-Roscher'), 20 * 60 * 1000);
+        if (services.githubStats)
+            setImmediateInterval(() => GitHubStatsService.updateGitHubStats(apiInstance, 'Rene-Roscher'), 20 * 60 * 1000);
 
         // Update GitHub contributions
         // setImmediateInterval(() => GitHubContributionsService.updateGitHubContributions(apiInstance, 'Rene-Roscher'), 20*60*1000);
